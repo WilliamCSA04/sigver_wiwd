4489bc0c57bf96a12589ab221d2330893ca3c3a8
William Caetano <williamcaetano14@gmail.com>
2018-09-06 22:37:26 -0300

    Moved code from knn_classifier to process_folder

--- process_folder.py
+++ process_folder.py
@@ -21,14 +21,22 @@ import sys
 import os
 import scipy.io
 
+from sklearn import neighbors
+
+
 if len(sys.argv) not in [4,6]:
     print('Usage: python process_folder.py <signatures_path> <save_path> '
           '<model_path> [canvas_size]')
     exit(1)
 
-signatures_path = sys.argv[1]
+signatures_folder = "0008/"
+
+dataset_path = sys.argv[1]
+signatures_path = dataset_path + signatures_folder
 save_path = sys.argv[2]
 model_path = sys.argv[3]
+
+
 if len(sys.argv) == 4:
     canvas_size = (952, 1360)  # Maximum signature size
 else:
@@ -42,19 +50,108 @@ print('Using canvas size: %s' % (canvas_size,))
 model_weight_path = 'models/signet.pkl'
 model = CNNModel(signet, model_weight_path)
 
-files = os.listdir(signatures_path)
+paths = os.listdir(signatures_path)
+files_signature = paths[:15]
+files_skilled = paths[15:]
+files_random = os.listdir(dataset_path)
 
 # Note: it there is a large number of signatures to process, it is faster to
 # process them in batches (i.e. use "get_feature_vector_multiple")
-for f in files:
+k = 1
+data = list()
+expected = list()
+
+print("Generate Train Set")
+print("Adding Genuine")
+count = 0
+for f in files_signature:
     # Load and pre-process the signature
     filename = os.path.join(signatures_path, f)
+    if(count == 10):
+        break
+    if("f" in f):
+        continue
     original = imread(filename, flatten=1)
     processed = preprocess_signature(original, canvas_size)
 
     # Use the CNN to extract features
     feature_vector = model.get_feature_vector(processed)
+    data.append(feature_vector[0])
+    count += 1
+    expected.append(0)
 
-    # Save in the matlab format
-    save_filename = os.path.join(save_path, os.path.splitext(f)[0] + '.mat')
-    scipy.io.savemat(save_filename, {'feature_vector':feature_vector})
+count = 0
+print("Adding Skilled")
+
+for index, f in enumerate(files_skilled):
+    # Load and pre-process the signature
+    filename = os.path.join(signatures_path, f)
+    if(count == 10):
+        break
+    if("f" not in f):
+        continue
+    original = imread(filename, flatten=1)
+    processed = preprocess_signature(original, canvas_size)
+    # Use the CNN to extract features
+    feature_vector = model.get_feature_vector(processed)
+    data.append(feature_vector[0])
+    count += 1
+    expected.append(1)
+
+print("Adding Random")
+
+for p in files_random:
+    validate_p = p + "/"
+    if(validate_p == signatures_folder):
+        continue
+    folder_path = dataset_path + p
+    folder = os.listdir(folder_path)
+    count = 0 
+    for f in folder:
+        # Load and pre-process the signature
+        if(count == 10):
+            break
+        if("f" in f):
+            continue
+        filename = os.path.join(folder_path, f)
+        original = imread(filename, flatten=1)
+        processed = preprocess_signature(original, canvas_size)
+
+        # Use the CNN to extract features
+        feature_vector = model.get_feature_vector(processed)
+        data.append(feature_vector[0])
+        count += 1
+        expected.append(1)
+
+
+
+data_train = np.array(data)
+
+
+data = list()
+
+count_g = 0
+count_s = 0
+print("Generate Test Set")
+for f in paths:
+    # Load and pre-process the signature
+    filename = os.path.join(signatures_path, f)
+    if(count_g == 5 and count_s == 15):
+        break
+    elif("f" not in f and count_g == 5):
+        continue
+    elif("f" in f and count_s == 15):
+        continue   
+    original = imread(filename, flatten=1)
+    processed = preprocess_signature(original, canvas_size)
+
+    # Use the CNN to extract features
+    feature_vector = model.get_feature_vector(processed)
+    data.append(feature_vector[0])
+    if("f" not in f):
+        count_g += 1
+    else:
+        count_s += 1
+    
+
+data_test = np.array(data)
