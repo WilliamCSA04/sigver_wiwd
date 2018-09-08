58ac94102726dd565a515377b3518c940a6cc17f
William Caetano <williamcaetano14@gmail.com>
2018-09-08 02:18:40 -0300

    Added tree classifier

--- classifier.py
+++ classifier.py
@@ -1,5 +1,5 @@
 from sklearn import neighbors
-
+from sklearn import tree
 
 def knn(data_train, data_test, expected, k = 1, weigths=['uniform', 'distance']):
     print("KNN Classifier")
@@ -11,3 +11,9 @@ def knn(data_train, data_test, expected, k = 1, weigths=['uniform', 'distance'])
         print("Prediction: " + weight) 
         print(prediction) 
 
+def tree(data_train, data_test, expeted):
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(data_train, expected)
+    prediction = clf.predict(data_test)
+    print("Prediction Tree: ") 
+    print(prediction)
