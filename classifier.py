e2a56432cef01527e91e1c5a3c7c666543f77fc7
William Caetano <williamcaetano14@gmail.com>
2018-09-08 02:23:50 -0300

    Added svm method to classifiers

--- classifier.py
+++ classifier.py
@@ -1,5 +1,6 @@
 from sklearn import neighbors
 from sklearn import tree
+from sklearn import svm
 
 def knn(data_train, data_test, expected, k = 1, weigths=['uniform', 'distance']):
     print("KNN Classifier")
@@ -11,9 +12,16 @@ def knn(data_train, data_test, expected, k = 1, weigths=['uniform', 'distance'])
         print("Prediction: " + weight) 
         print(prediction) 
 
-def tree(data_train, data_test, expeted):
+def tree(data_train, data_test, expected):
     clf = tree.DecisionTreeClassifier()
     clf = clf.fit(data_train, expected)
+    prediction = clf.predict(data_test)
+    print("Prediction Tree: ") 
+    print(prediction)
+
+def svm(data_train, data_test, expected):
+    clf = svm.SVC()
+    clf.fit(data_train, expected)
     prediction = clf.predict(data_test)
     print("Prediction Tree: ") 
     print(prediction)
