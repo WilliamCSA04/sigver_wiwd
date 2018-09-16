6f30de6bb5e5efe96a6d52d7734ed091e0fc857f
Williamcsa04 <williamcaetano14@gmail.com>
2018-09-16 18:24:48 -0300

    Added lower() call to all filters

--- process_helper.py
+++ process_helper.py
@@ -1,13 +1,13 @@
 import os
 
 def filter_forgery(text):
-    return "f" in text
+    return "f" in text.lower()
 
 def filter_genuine(text):
-    return "f" not in text
+    return "f" not in text.lower()
 
 def filter_by_text(text_full, text_part):
-    return text_part in text_full
+    return text_part.lower() in text_full.lower()
 
 def remove_invalid_files(name):
-    return ".bmp" in name or ".jpg" in name
+    return ".bmp" in name.lower() or ".jpg" in name.lower()
