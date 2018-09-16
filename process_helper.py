8940a43abcffd3dda0a27c5bb3614519a0599de8
Williamcsa04 <williamcaetano14@gmail.com>
2018-09-16 05:20:58 -0300

    Created new filters

--- process_helper.py
+++ process_helper.py
@@ -7,7 +7,14 @@ def filter_array_of_folders(folders, path):
         files = filter(remove_invalid_files, files)
     return folders
 
+def filter_forgery(text):
+    return "f" in text
 
+def filter_genuine(text):
+    return "f" not in text
+
+def filter_by_text(text_full, text_part):
+    return text_part in text_full
 
 def remove_invalid_files(name):
     return ".bmp" in name or ".jpg" in name
