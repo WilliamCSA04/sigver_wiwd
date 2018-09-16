517c6bd696ee5477b6fdcceb7ea117831ade2e8f
Williamcsa04 <williamcaetano14@gmail.com>
2018-09-16 03:05:39 -0300

    Added default beginning values to datasets, model  and canvas size

--- process_datasets.py
+++ process_datasets.py
@@ -8,3 +8,7 @@ import os
 import scipy.io
 import classifier
 import random
+
+datasets_path = ["datasets/MCYT/", "datasets/GPDS160/", "datasets/GPDS300/"] #All datasets needed
+model_path = "models/signet.pkl" #Always will use this model
+canvas_size = (952, 1360)  # Maximum signature size
