eb5983bd858dc24d5dd38c2ae645fef34c544ddb
Williamcsa04 <williamcaetano14@gmail.com>
2018-09-16 03:12:01 -0300

    Added list of folders for each dataset

--- process_datasets.py
+++ process_datasets.py
@@ -9,8 +9,15 @@ import scipy.io
 import classifier
 import random
 
-datasets_path = ["datasets/MCYT/", "datasets/GPDS160/", "datasets/GPDS300/"] #All datasets needed
+datasets_paths = ["datasets/MCYT/", "datasets/GPDS160/", "datasets/GPDS300/"] #All datasets needed
 model_path = "models/signet.pkl" #Always will use this model
 canvas_size = (952, 1360)  # Maximum signature size
 
 model = CNNModel(signet, model_path)
+
+print("Loading MCYT-75")
+mcyt_folders = os.listdir(datasets_paths[0])
+print("Loading GPDS-160")
+gpds_160_folders = os.listdir(datasets_paths[1])
+print("Loading GPDS-300")
+gpds_300_folders = os.listdir(datasets_paths[2])
