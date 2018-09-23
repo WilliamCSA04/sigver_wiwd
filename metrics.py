f9a1645559ec366e346de4b582baa70ede836811
Williamcsa04 <williamcaetano14@gmail.com>
2018-09-22 21:17:59 -0300

    Created standard derivation

--- metrics.py
+++ metrics.py
@@ -1,6 +1,17 @@
+import math
+
 def average(array_of_number):
     sum = 0
     for number in array_of_number:
         sum += number
     avg = sum/float(len(array_of_number))
     return avg
+
+def standard_deviation(array_of_number, avg = None):
+    sum = 0
+    if(avg is None):
+        avg = average(array_of_number)
+    for i in array_of_number:
+        sum += pow(i - avg, 2)
+    sd = math.sqrt(sum/float(len(array_of_number)))
+    return sd
