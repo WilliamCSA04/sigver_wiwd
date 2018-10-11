fcf82e0f8c679861263aa12d04370b3cd41dc1b5
Williamcsa04 <williamcaetano14@gmail.com>
2018-10-11 04:26:14 -0300

    Added interation to test all users instead of only one

--- process_datasets.py
+++ process_datasets.py
@@ -18,10 +18,12 @@ model_path = "models/signet.pkl" #Always will use this model
 canvas_size = (1768, 2176)  # Maximum signature size
 canvas = []
 dataset = ""
+number_of_interations = 1
 if(len(sys.argv) == 1):
     datasets_paths = ["datasets/MCYT/", "datasets/GPDS160/", "datasets/GPDS300/"]#All datasets needed
 else:
     dataset = sys.argv[1]
+    number_of_interations = int(sys.argv[2])
     datasets_paths = ["datasets/"+ dataset +"/"]
 
 model = CNNModel(signet, model_path)
@@ -48,150 +50,151 @@ gpds_300_folders = os.listdir(gpds_300_path)
 gpds_300_folders = [folder + "/" for folder in gpds_300_folders]
 gpds_300_genuine_candidates = list(gpds_300_folders)
 
-if(dataset == "MCYT"  or dataset == ""):
-    print("Loading MCYT")
-    #For each array in next line is [number_of_samples_for_train, number_of_samples_for_test]
-    mcyt_genuine_options=[10, 5]
-    mcyt_forgery_options=[0, 15]
-    mcyt_random_options=[10, 0]
-    mcyt_sets_classification = split_into_train_test(mcyt_folders, mcyt_genuine_candidates[0], mcyt_path, mcyt_genuine_options[0], mcyt_forgery_options[0], mcyt_random_options[0])
-    mcyt_sets = mcyt_sets_classification[0]
-    mcyt_train_set = mcyt_sets[0]
-    mcyt_test_set = mcyt_sets[1]
-    validate_train_test(mcyt_train_set, mcyt_test_set)
-    mcyt_train_classification = mcyt_sets_classification[1]
-    train_sets.append(mcyt_train_set)
-    test_sets.append(mcyt_test_set)
-    train_message.append("Starting preprocess images for train of MCYT")
-    test_message.append("Starting preprocess images for test of MCYT")
-    classifications.append(mcyt_train_classification)
-    options.append([mcyt_forgery_options[1], mcyt_random_options[1]])
-    canvas.append((600, 850))
-    svm_genuine_weight = (mcyt_random_options[0]*74)/mcyt_genuine_options[0]
-    svm_weights.append({0: 1, 1: svm_genuine_weight})
-    print("This dataset has for test: genuine samples: " + str(len(mcyt_test_set[0])) + " ,Forgery: " + str(len(mcyt_test_set[1])) + " ,Random: " + str(len(mcyt_test_set[2])))
+for number_of_interation in range(number_of_interations):
+    print("Number of interation:" + str(number_of_interation))
+    if(dataset == "MCYT"  or dataset == ""):
+        print("Loading MCYT")
+        #For each array in next line is [number_of_samples_for_train, number_of_samples_for_test]
+        mcyt_genuine_options=[10, 5]
+        mcyt_forgery_options=[0, 15]
+        mcyt_random_options=[10, 0]
+        mcyt_sets_classification = split_into_train_test(mcyt_folders, mcyt_genuine_candidates.pop(), mcyt_path, mcyt_genuine_options[0], mcyt_forgery_options[0], mcyt_random_options[0])
+        mcyt_sets = mcyt_sets_classification[0]
+        mcyt_train_set = mcyt_sets[0]
+        mcyt_test_set = mcyt_sets[1]
+        validate_train_test(mcyt_train_set, mcyt_test_set)
+        mcyt_train_classification = mcyt_sets_classification[1]
+        train_sets.append(mcyt_train_set)
+        test_sets.append(mcyt_test_set)
+        train_message.append("Starting preprocess images for train of MCYT")
+        test_message.append("Starting preprocess images for test of MCYT")
+        classifications.append(mcyt_train_classification)
+        options.append([mcyt_forgery_options[1], mcyt_random_options[1]])
+        canvas.append((600, 850))
+        svm_genuine_weight = (mcyt_random_options[0]*74)/mcyt_genuine_options[0]
+        svm_weights.append({0: 1, 1: svm_genuine_weight})
+        print("This dataset has for test: genuine samples: " + str(len(mcyt_test_set[0])) + " ,Forgery: " + str(len(mcyt_test_set[1])) + " ,Random: " + str(len(mcyt_test_set[2])))
+    if(dataset == "GPDS160" or dataset == ""):
+        print("Loading GPDS-160")
+        gpds_160_genuine_options = [14, 10]
+        gpds_160_forgery_options = [0, 10]
+        gpds_160_random_options = [14, 10]
+        gpds_160_sets = split_into_train_test(gpds_160_folders, gpds_160_genuine_candidates[0], gpds_160_path, gpds_160_genuine_options[0], gpds_160_forgery_options[0], gpds_160_random_options[0])
+        gpds_160_train_set = gpds_160_sets[0][0]
+        gpds_160_test_set = gpds_160_sets[0][1]
+        validate_train_test(gpds_160_train_set, gpds_160_test_set)
+        train_message.append("Starting preprocess images for train of GPDS160")
+        test_message.append("Starting preprocess images for test of GPDS160")
+        gpds_160_train_classification = gpds_160_sets[1]
+        train_sets.append(gpds_160_train_set)
+        test_sets.append(gpds_160_test_set)
+        classifications.append(gpds_160_train_classification)
+        options.append([gpds_160_forgery_options[1], gpds_160_random_options[1]])
+        canvas.append((1768, 2176))
+        svm_genuine_weight = (gpds_160_random_options[0]*159)/gpds_160_genuine_options[0]
+        svm_weights.append({0: 1, 1: svm_genuine_weight})
+        print("This dataset has for test: genuine samples: " + str(len(gpds_160_test_set[0])) + " ,Forgery: " + str(len(gpds_160_test_set[1])) + " ,Random: " + str(len(gpds_160_test_set[2])))
 
-if(dataset == "GPDS160" or dataset == ""):
-    print("Loading GPDS-160")
-    gpds_160_genuine_options = [14, 10]
-    gpds_160_forgery_options = [0, 10]
-    gpds_160_random_options = [14, 10]
-    gpds_160_sets = split_into_train_test(gpds_160_folders, gpds_160_genuine_candidates[0], gpds_160_path, gpds_160_genuine_options[0], gpds_160_forgery_options[0], gpds_160_random_options[0])
-    gpds_160_train_set = gpds_160_sets[0][0]
-    gpds_160_test_set = gpds_160_sets[0][1]
-    validate_train_test(gpds_160_train_set, gpds_160_test_set)
-    train_message.append("Starting preprocess images for train of GPDS160")
-    test_message.append("Starting preprocess images for test of GPDS160")
-    gpds_160_train_classification = gpds_160_sets[1]
-    train_sets.append(gpds_160_train_set)
-    test_sets.append(gpds_160_test_set)
-    classifications.append(gpds_160_train_classification)
-    options.append([gpds_160_forgery_options[1], gpds_160_random_options[1]])
-    canvas.append((1768, 2176))
-    svm_genuine_weight = (gpds_160_random_options[0]*159)/gpds_160_genuine_options[0]
-    svm_weights.append({0: 1, 1: svm_genuine_weight})
-    print("This dataset has for test: genuine samples: " + str(len(gpds_160_test_set[0])) + " ,Forgery: " + str(len(gpds_160_test_set[1])) + " ,Random: " + str(len(gpds_160_test_set[2])))
+    if(dataset == "GPDS300" or dataset == ""):
+        print("Loading GPDS-300")
+        gpds_300_genuine_options = [14, 10]
+        gpds_300_forgery_options = [0, 10]
+        gpds_300_random_options = [14, 10]
+        gpds_300_sets = split_into_train_test(gpds_300_folders, gpds_300_genuine_candidates[0], gpds_300_path, gpds_300_genuine_options[0], gpds_300_forgery_options[0], gpds_300_random_options[0])
+        gpds_300_train_set = gpds_300_sets[0][0]
+        gpds_300_test_set = gpds_300_sets[0][1]
+        validate_train_test(gpds_300_train_set, gpds_300_test_set)
+        train_message.append("Starting preprocess images for train of GPDS300")
+        test_message.append("Starting preprocess images for test of GPDS300")
+        gpds_300_train_classification = gpds_300_sets[1]
+        train_sets.append(gpds_300_train_set)
+        test_sets.append(gpds_300_test_set)
+        classifications.append(gpds_300_train_classification)
+        options.append([gpds_300_forgery_options[1], gpds_300_random_options[1]])
+        canvas.append((1768, 2176))
+        svm_genuine_weight = (gpds_300_random_options[0]*299)/gpds_300_genuine_options[0]
+        svm_weights.append({0: 1, 1: svm_genuine_weight})
+        print("This dataset has for test: genuine samples: " + str(len(gpds_300_test_set[0])) + " ,Forgery: " + str(len(gpds_300_test_set[1])) + " ,Random: " + str(len(gpds_300_test_set[2])))
 
-if(dataset == "GPDS300" or dataset == ""):
-    print("Loading GPDS-300")
-    gpds_300_genuine_options = [14, 10]
-    gpds_300_forgery_options = [0, 10]
-    gpds_300_random_options = [14, 10]
-    gpds_300_sets = split_into_train_test(gpds_300_folders, gpds_300_genuine_candidates[0], gpds_300_path, gpds_300_genuine_options[0], gpds_300_forgery_options[0], gpds_300_random_options[0])
-    gpds_300_train_set = gpds_300_sets[0][0]
-    gpds_300_test_set = gpds_300_sets[0][1]
-    validate_train_test(gpds_300_train_set, gpds_300_test_set)
-    train_message.append("Starting preprocess images for train of GPDS300")
-    test_message.append("Starting preprocess images for test of GPDS300")
-    gpds_300_train_classification = gpds_300_sets[1]
-    train_sets.append(gpds_300_train_set)
-    test_sets.append(gpds_300_test_set)
-    classifications.append(gpds_300_train_classification)
-    options.append([gpds_300_forgery_options[1], gpds_300_random_options[1]])
-    canvas.append((1768, 2176))
-    svm_genuine_weight = (gpds_300_random_options[0]*299)/gpds_300_genuine_options[0]
-    svm_weights.append({0: 1, 1: svm_genuine_weight})
-    print("This dataset has for test: genuine samples: " + str(len(gpds_300_test_set[0])) + " ,Forgery: " + str(len(gpds_300_test_set[1])) + " ,Random: " + str(len(gpds_300_test_set[2])))
-
-train_sets_processed = [[],[],[]]
-for index, set in enumerate(train_sets):
-    print(train_message[index])
-    for image in set:
-        original = imread(image, flatten=1)
-        processed = preprocess_signature(original, canvas[index])
-        train_sets_processed[index].append(model.get_feature_vector(processed)[0])
-
-for i, test_set in enumerate(test_sets):
-    print(test_message[i])
-    genuine_for_test = []
-    forgery_for_test = []
-    random_for_test = []
-    for index, set in enumerate(test_set):
+    train_sets_processed = [[],[],[]]
+    for index, set in enumerate(train_sets):
+        print(train_message[index])
         for image in set:
             original = imread(image, flatten=1)
-            if(index == 0):
-                processed = preprocess_signature(original, canvas[i])
-                feature_vector = model.get_feature_vector(processed)
-                genuine_for_test.append(feature_vector[0])
-            elif(index == 1):
-                processed = preprocess_signature(original, canvas[i])
-                feature_vector = model.get_feature_vector(processed)
-                forgery_for_test.append(feature_vector[0])
-            else:
-                random_for_test.append(original)
-    far_metrics = [[],[],[],[]]
-    frr_skilled_metrics = [[],[],[],[]]
-    far_random_metrics = [[],[],[],[]]
-    eer_metrics = [[],[],[],[]]
-    for j in range(100):
-        print("Interation: " + str(j))
-        random.shuffle(forgery_for_test)
-        random.shuffle(random_for_test)
-        option = options[i]
-        random_signatures_for_test = [model.get_feature_vector(preprocess_signature(original, canvas[i]))[0] for original in random_for_test[:option[1]]]
-        test = genuine_for_test + forgery_for_test[:option[0]] + random_signatures_for_test
-        test_classification = []
-        genuine_quantity = len(genuine_for_test)
-        for k in range(genuine_quantity):
-            test_classification.append(1)
-        for k in range(option[0] + option[1]):
-            test_classification.append(0)
-        metrics = classifier.knn(np.array(train_sets_processed[i]), test, classifications[i], test_classification, genuine_quantity, option[0], option[1])
-        far_metrics[0].append(metrics[0])
-        frr_skilled_metrics[0].append(metrics[1])
-        far_random_metrics[0].append(metrics[2])
-        eer_metrics[0].append(metrics[3])
+            processed = preprocess_signature(original, canvas[index])
+            train_sets_processed[index].append(model.get_feature_vector(processed)[0])
 
-        metrics = classifier.tree(np.array(train_sets_processed[i]), test, classifications[i], test_classification, genuine_quantity, option[0], option[1])
-        far_metrics[1].append(metrics[0])
-        frr_skilled_metrics[1].append(metrics[1])
-        far_random_metrics[1].append(metrics[2])
-        eer_metrics[1].append(metrics[3])
+    for i, test_set in enumerate(test_sets):
+        print(test_message[i])
+        genuine_for_test = []
+        forgery_for_test = []
+        random_for_test = []
+        for index, set in enumerate(test_set):
+            for image in set:
+                original = imread(image, flatten=1)
+                if(index == 0):
+                    processed = preprocess_signature(original, canvas[i])
+                    feature_vector = model.get_feature_vector(processed)
+                    genuine_for_test.append(feature_vector[0])
+                elif(index == 1):
+                    processed = preprocess_signature(original, canvas[i])
+                    feature_vector = model.get_feature_vector(processed)
+                    forgery_for_test.append(feature_vector[0])
+                else:
+                    random_for_test.append(original)
+        far_metrics = [[],[],[],[]]
+        frr_skilled_metrics = [[],[],[],[]]
+        far_random_metrics = [[],[],[],[]]
+        eer_metrics = [[],[],[],[]]
+        for j in range(100):
+            print("Interation: " + str(j))
+            random.shuffle(forgery_for_test)
+            random.shuffle(random_for_test)
+            option = options[i]
+            random_signatures_for_test = [model.get_feature_vector(preprocess_signature(original, canvas[i]))[0] for original in random_for_test[:option[1]]]
+            test = genuine_for_test + forgery_for_test[:option[0]] + random_signatures_for_test
+            test_classification = []
+            genuine_quantity = len(genuine_for_test)
+            for k in range(genuine_quantity):
+                test_classification.append(1)
+            for k in range(option[0] + option[1]):
+                test_classification.append(0)
+            metrics = classifier.knn(np.array(train_sets_processed[i]), test, classifications[i], test_classification, genuine_quantity, option[0], option[1])
+            far_metrics[0].append(metrics[0])
+            frr_skilled_metrics[0].append(metrics[1])
+            far_random_metrics[0].append(metrics[2])
+            eer_metrics[0].append(metrics[3])
 
-        metrics = classifier.svm(np.array(train_sets_processed[i]), test, classifications[i], test_classification, genuine_quantity, option[0], option[1], weights=svm_weights[i])
-        far_metrics[2].append(metrics[0])
-        frr_skilled_metrics[2].append(metrics[1])
-        far_random_metrics[2].append(metrics[2])
-        eer_metrics[2].append(metrics[3])
+            metrics = classifier.tree(np.array(train_sets_processed[i]), test, classifications[i], test_classification, genuine_quantity, option[0], option[1])
+            far_metrics[1].append(metrics[0])
+            frr_skilled_metrics[1].append(metrics[1])
+            far_random_metrics[1].append(metrics[2])
+            eer_metrics[1].append(metrics[3])
 
-        metrics = classifier.mlp(np.array(train_sets_processed[i]), test, classifications[i], test_classification, genuine_quantity, option[0], option[1])
-        far_metrics[3].append(metrics[0])
-        frr_skilled_metrics[3].append(metrics[1])
-        far_random_metrics[3].append(metrics[2])
-        eer_metrics[3].append(metrics[3])
+            metrics = classifier.svm(np.array(train_sets_processed[i]), test, classifications[i], test_classification, genuine_quantity, option[0], option[1], weights=svm_weights[i])
+            far_metrics[2].append(metrics[0])
+            frr_skilled_metrics[2].append(metrics[1])
+            far_random_metrics[2].append(metrics[2])
+            eer_metrics[2].append(metrics[3])
 
-    for p in range(4):
-        types = ["KNN", "Tree", "SVM", "MLP"]
-        print("averages " + types[p])
-        print(average(far_metrics[p]))
-        print(average(frr_skilled_metrics[p]))
-        print(average(far_random_metrics[p]))
-        print(average(eer_metrics[p]))
-        print("standard deviations " + types[p])
-        print(standard_deviation(far_metrics[p]))
-        print(standard_deviation(frr_skilled_metrics[p]))
-        print(standard_deviation(far_random_metrics[p]))
-        print(standard_deviation(eer_metrics[p]))
+            metrics = classifier.mlp(np.array(train_sets_processed[i]), test, classifications[i], test_classification, genuine_quantity, option[0], option[1])
+            far_metrics[3].append(metrics[0])
+            frr_skilled_metrics[3].append(metrics[1])
+            far_random_metrics[3].append(metrics[2])
+            eer_metrics[3].append(metrics[3])
 
-    
+        for p in range(4):
+            types = ["KNN", "Tree", "SVM", "MLP"]
+            print("averages " + types[p])
+            print(average(far_metrics[p]))
+            print(average(frr_skilled_metrics[p]))
+            print(average(far_random_metrics[p]))
+            print(average(eer_metrics[p]))
+            print("standard deviations " + types[p])
+            print(standard_deviation(far_metrics[p]))
+            print(standard_deviation(frr_skilled_metrics[p]))
+            print(standard_deviation(far_random_metrics[p]))
+            print(standard_deviation(eer_metrics[p]))
+
+        
 
