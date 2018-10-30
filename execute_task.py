e2d18917516d74241774a5c731aafb6c360859b2
Williamcsa04 <williamcaetano14@gmail.com>
2018-10-30 04:59:07 -0200

    Added process for genuine images

--- execute_task.py
+++ execute_task.py
@@ -1,5 +1,9 @@
 import sys
+import signet
+from cnn_model import CNNModel
 from gpds_signatures import *
+from signature_image_features import add_feature_vector_from_a_image
+
 dataset = sys.argv[1]
 
 config = {}
@@ -27,5 +31,25 @@ test_set = {
     "random": []
 }
 
+svm = config["svm_linear"]
+
+model = CNNModel(signet, svm["model_path"])
+images_dictionary = {}
+
 train_genuine_users = get_signature_folders(config["dataset_path"])
+print("Loading list for genuine users to train")
 train_random_users = get_signature_folders(config["dataset_for_random_path"])
+print("Loading list for random users to train")
+
+print("Starting preprocess genuine signatures for train")
+for user in train_genuine_users:
+    path = config["dataset_path"] + user
+    train_config = config["train_config"]
+    genuine_signatures = get_genuines(path, train_config["genuine"])
+    for image in genuine_signatures:
+        add_feature_vector_from_a_image(images_dictionary, image, config["max_image_size"], config["canvas"], train_set["genuine"], model)
+
+print("Train set:")
+print("Train Genuines: " + str(len(train_set["genuine"])))
+print("Train Skilled: " + str(len(train_set["skilled"])))
+print("Train Random: " + str(len(train_set["random"])))
