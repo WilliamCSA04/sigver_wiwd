1dd52228327812c820fa43214304d48bba0a74fa
Williamcsa04 <williamcaetano14@gmail.com>
2018-10-30 02:43:33 -0200

    Added gpds config

--- GPDS_config.py
+++ GPDS_config.py
@@ -1,3 +1,35 @@
 def gpds160_config():
-    config = {}
+    config = {
+        "dataset_genuine_path": "/datasets/gpds160/",
+        "dataset_random_path": "/datasets/gpds160-RANDOM/",
+        "train_config": {
+            "genuine": 14,
+            "skilled": 0,
+            "random": 14
+        },
+        "test_config": {
+            "genuine": 10,
+            "skilled": 10,
+            "random": 10
+        },
+        "number_of_interations": 160
+    }
+    return config
+
+def gpds300_config():
+    config = {
+        "dataset_genuine_path": "/datasets/gpds300/",
+        "dataset_random_path": "/datasets/gpds300-RANDOM/",
+        "train_config": {
+            "genuine": 14,
+            "skilled": 0,
+            "random": 14
+        },
+        "test_config": {
+            "genuine": 10,
+            "skilled": 10,
+            "random": 10
+        },
+        "number_of_interations": 300
+    }
     return config
