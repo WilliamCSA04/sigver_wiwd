7e45c030b1711a608d5ecdbf8608d818e9a9d386
Williamcsa04 <williamcaetano14@gmail.com>
2018-10-30 03:14:01 -0200

    Added filter_signatures method

--- gpds_signatures.py
+++ gpds_signatures.py
@@ -6,20 +6,22 @@ def get_signature_folders(path):
 
 def get_genuines(path, number_of_signatures):
     signatures = __get_signatures(path)
-    __validate_number_of_signatures(len(signatures), number_of_signatures)
-    filtered_signatures = filter(filter_genuine, signatures)[:number_of_signatures]
+    filtered_signatures = __filter_signatures(filter_genuine, signatures, number_of_signatures)
     return filtered_signatures
 
 def get_skilled(path, number_of_signatures):
     signatures = __get_signatures(path)
-    __validate_number_of_signatures(len(signatures), number_of_signatures)
-    filtered_signatures = filter(filter_forgery, signatures)[:number_of_signatures]
+    filtered_signatures = __filter_signatures(filter_forgery, signatures, number_of_signatures)
     return filtered_signatures
 
 def __get_signatures(path):
     signatures = os.listdir(path)
     filtered_signatures = filter(remove_invalid_files, signatures)
     return filtered_signatures
+
+def __filter_signatures(filter_method, signatures, number_of_signatures):
+    __validate_number_of_signatures(len(signatures), number_of_signatures)
+    return filter(filter_method, signatures)[:number_of_signatures]
 
 def __validate_number_of_signatures(max_limit, number_of_signatures):
     min_limit = 0
