4e2f92e49963ac4a60614332234abda4e9279347
Williamcsa04 <williamcaetano14@gmail.com>
2018-10-30 07:14:17 -0200

    Added random images for test

--- execute_task.py
+++ execute_task.py
@@ -1,5 +1,6 @@
 import sys
 import signet
+import random
 from cnn_model import CNNModel
 from gpds_signatures import *
 from signature_image_features import add_feature_vector_from_a_image
@@ -25,24 +26,20 @@ train_set = {
     "random": []
 }
 
-test_set = {
-    "genuines": [],
-    "skilled": [],
-    "random": []
-}
+
 
 svm = config["svm_linear"]
 
 model = CNNModel(signet, svm["model_path"])
 images_dictionary = {}
 
-train_random_users = get_signature_folders(config["dataset_for_random_path"])
+random_users = get_signature_folders(config["dataset_for_random_path"])
 print("Loading list for random users to train")
 
 train_config = config["train_config"]
 
 print("Starting preprocess random signatures for train")
-for user in train_random_users:
+for user in random_users:
     path = config["dataset_for_random_path"] + user
     random_signatures = get_genuines(path, train_config["random"])
     for image in random_signatures:
@@ -67,7 +64,11 @@ for user in train_genuine_users:
     print("Train Genuines: " + str(len(train_set["genuines"])))
     print("Train Skilled: " + str(len(train_set["skilled"])))
     print("Train Random: " + str(len(train_set["random"])))
-
+    test_set = {
+        "genuines": [],
+        "skilled": [],
+        "random": []
+    }
     max_signature_numbers = config["signature_numbers_by_user"]
     test_config = config["test_config"]
     print("Loading genuine signatures to test")
@@ -84,7 +85,20 @@ for user in train_genuine_users:
         image_path = path+"/"+image
         add_feature_vector_from_a_image(images_dictionary, image_path, config["max_image_size"], config["canvas"], test_set["skilled"], model)
     
+    print("Loading random signatures to test")
     
+    temp_random_images = []
+    for user in random_users:
+        path = config["dataset_for_random_path"] + user
+        random_signatures = get_genuines(path, max_signature_numbers["genuine"])[genuine_for_train:]
+        for image in random_signatures:
+            image_path = path+"/"+image
+            temp_random_images.append(image_path)
+
+    random.shuffle(temp_random_images)
+    for image_path in temp_random_images[:test_config["random"]]:
+        add_feature_vector_from_a_image(images_dictionary, image_path, config["max_image_size"], config["canvas"], test_set["random"], model)
+
     print("Test set:")
     print("Test Genuines: " + str(len(test_set["genuines"])))
     print("Test Skilled: " + str(len(test_set["skilled"])))
