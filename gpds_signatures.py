ab523eb284af979c98243f334d3f525efe3d8694
Williamcsa04 <williamcaetano14@gmail.com>
2018-11-04 21:17:08 -0200

    Added shuffle to filter_signature method

--- gpds_signatures.py
+++ gpds_signatures.py
@@ -1,5 +1,6 @@
 import os
 from process_helper import filter_genuine, filter_forgery, remove_invalid_files
+import random
 
 def get_signature_folders(path):
     return os.listdir(path)
@@ -11,7 +12,7 @@ def get_genuines(path, number_of_signatures):
 
 def get_skilled(path, number_of_signatures):
     signatures = __get_signatures(path)
-    filtered_signatures = __filter_signatures(filter_forgery, signatures, number_of_signatures)
+    filtered_signatures = __filter_signatures(filter_forgery, signatures, number_of_signatures, should_shuffle = True)
     return filtered_signatures
 
 def __get_signatures(path):
@@ -19,8 +20,13 @@ def __get_signatures(path):
     filtered_signatures = filter(remove_invalid_files, signatures)
     return filtered_signatures
 
-def __filter_signatures(filter_method, signatures, number_of_signatures):
+def __filter_signatures(filter_method, signatures, number_of_signatures, should_shuffle = False):
     __validate_number_of_signatures(len(signatures), number_of_signatures)
+    if should_shuffle:
+        filtered_signatures = filter(filter_method, signatures)
+        random.shuffle(filtered_signatures)
+        return filtered_signatures[:number_of_signatures]
+    
     return filter(filter_method, signatures)[:number_of_signatures]
 
 def __validate_number_of_signatures(max_limit, number_of_signatures):
